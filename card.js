13838372eb1bfc11b2f1c958acb575aa6123ef19
NilsHolthey <nils.holthey@gmx.de>
2022-04-04 09:31:50 +0200

    add html structure

--- card.js
+++ card.js
@@ -1,21 +1,3 @@
-// export default function Card (cardElement) {
-//   const showButton = document.querySelector(".js-show__button1");
-//   const hideButton = document.querySelector(".js-hide__button1");
-//   const answer = document.querySelector(".js-answer__box1");
-
-//   showButton.addEventListener("click", () => {
-//     answer.classList.add("answer--active");
-//     showButton.classList.add("button__show--active");
-//     hideButton.classList.add("button__hide--active");
-//   });
-
-//   hideButton.addEventListener("click", () => {
-//     answer.classList.remove("answer--active");
-//     showButton.classList.remove("button__show--active");
-//     hideButton.classList.remove("button__hide--active");
-//   });
-// }
-
 export default function Quizcard(cardElement) {
   const quizCardBtn = cardElement.querySelector(".js-show__button1");
 
@@ -31,8 +13,10 @@ export default function Quizcard(cardElement) {
     if (showAnswer) {
       quizCardBtn.textContent = "HIDE ANSWER";
       cardAnswer.classList.add("answer--active");
+      quizCardBtn.classList.add("button__show_color");
     } else {
       quizCardBtn.textContent = "SHOW ANSWER";
+      quizCardBtn.classList.remove("button__show_color");
       cardAnswer.classList.remove("answer--active");
     }
   });
--- index.js
+++ index.js
@@ -5,22 +5,3 @@ const cardElements = document.querySelectorAll(".js-box");
 cardElements.forEach((cardElement) => {
   Quizcard(cardElement);
 });
-
-// import bookmarFunction from "./bookmark.js";
-
-// import Card from "./card.js";
-
-// const cardElements = document.querySelectorAll(".js-box");
-
-// console.log(cardElements);
-// cardElements.forEach((cardElement) => {
-//   Card(cardElement);
-// });
-
-// const Bookmarks = document.querySelectorAll(".js-bookmark__box1");
-
-// Bookmarks.forEach((bookmarkButton) => {
-//   bookmarFunction(bookmarkButton);
-// });
-
-// console.log(cardElements);
